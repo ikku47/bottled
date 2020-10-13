777884ce905279d11e16fb034b0053f80281c611
ikku47 <iqbal.pk@acodez.co.in>
2020-10-13 20:42:00 +0530

    build

---
+++ my-app/build/precache-manifest.4628292d01c0a3ebf85884d26ab81017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9546360c8e2c8fa2b5a451e18c54fa02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c14525c8dda88eda8ecb",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9dab379099bb9b861ebe",
+    "url": "/static/css/main.1ea25625.chunk.css"
+  },
+  {
+    "revision": "c14525c8dda88eda8ecb",
+    "url": "/static/js/2.0db541d0.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "/static/js/2.0db541d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dab379099bb9b861ebe",
+    "url": "/static/js/main.1fa1c610.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
